a0179ef8b0665c028d2c25f4bcc1c98c3a2aea51
liuzhihuazyit <130758692+liuzhihuazyit@users.noreply.github.com>
2024-03-23 17:56:14 +0800

    Add files via upload
    add rtuovertcp

---
+++ rtuovertcp.go
@@ -0,0 +1,283 @@
+package modbus
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+// TCPClientHandler implements Packager and Transporter interface.
+type RtuOverTCPClientHandler struct {
+	rtuovertcpPackager
+	rtuovertcpTransporter
+}
+
+// NewTCPClientHandler allocates a new TCPClientHandler.
+func NewRTUOverTCPClientHandler(address string) *RtuOverTCPClientHandler {
+	h := &RtuOverTCPClientHandler{}
+	h.Address = address
+	h.Timeout = tcpTimeout
+	h.IdleTimeout = tcpIdleTimeout
+	return h
+}
+
+// TCPClient creates TCP client with default handler and given connect string.
+func RtuOverTCPClient(address string) Client {
+	handler := NewRTUOverTCPClientHandler(address)
+	return NewClient(handler)
+}
+
+// tcpPackager implements Packager interface.
+type rtuovertcpPackager struct {
+	// For synchronization between messages of server & client
+	transactionId uint32
+	// Broadcast address is 0
+	SlaveId byte
+}
+
+// Encode adds modbus application protocol header:
+//  Transaction identifier: 2 bytes
+//  Protocol identifier: 2 bytes
+//  Length: 2 bytes
+//  Unit identifier: 1 byte
+//  Function code: 1 byte
+//  Data: n bytes
+func (mb *rtuovertcpPackager) Encode(pdu *ProtocolDataUnit) (adu []byte, err error) {
+	length := len(pdu.Data) + 4
+	if length > rtuMaxSize {
+		err = fmt.Errorf("modbus: length of data '%v' must not be bigger than '%v'", length, rtuMaxSize)
+		return
+	}
+	adu = make([]byte, length)
+
+	adu[0] = mb.SlaveId
+	adu[1] = pdu.FunctionCode
+	copy(adu[2:], pdu.Data)
+
+	// Append crc
+	var crc crc
+	crc.reset().pushBytes(adu[0 : length-2])
+	checksum := crc.value()
+
+	adu[length-1] = byte(checksum >> 8)
+	adu[length-2] = byte(checksum)
+	return
+}
+
+// Verify confirms transaction, protocol and unit id.
+func (mb *rtuovertcpPackager) Verify(aduRequest []byte, aduResponse []byte) (err error) {
+	length := len(aduResponse)
+	// Minimum size (including address, function and CRC)
+	if length < rtuMinSize {
+		err = fmt.Errorf("modbus: response length '%v' does not meet minimum '%v'", length, rtuMinSize)
+		return
+	}
+	// Slave address must match
+	if aduResponse[0] != aduRequest[0] {
+		err = fmt.Errorf("modbus: response slave id '%v' does not match request '%v'", aduResponse[0], aduRequest[0])
+		return
+	}
+	return
+}
+
+// Decode extracts PDU from TCP frame:
+//  Transaction identifier: 2 bytes
+//  Protocol identifier: 2 bytes
+//  Length: 2 bytes
+//  Unit identifier: 1 byte
+func (mb *rtuovertcpPackager) Decode(adu []byte) (pdu *ProtocolDataUnit, err error) {
+	length := len(adu)
+	// Calculate checksum
+	var crc crc
+	crc.reset().pushBytes(adu[0 : length-2])
+	checksum := uint16(adu[length-1])<<8 | uint16(adu[length-2])
+	if checksum != crc.value() {
+		err = fmt.Errorf("modbus: response crc '%v' does not match expected '%v'", checksum, crc.value())
+		return
+	}
+	// Function code & data
+	pdu = &ProtocolDataUnit{}
+	pdu.FunctionCode = adu[1]
+	pdu.Data = adu[2 : length-2]
+	return
+}
+
+// tcpTransporter implements Transporter interface.
+type rtuovertcpTransporter struct {
+	// Connect string
+	Address string
+	// Connect & Read timeout
+	Timeout time.Duration
+	// Idle timeout to close the connection
+	IdleTimeout time.Duration
+	// Transmission logger
+	Logger *log.Logger
+
+	// TCP connection
+	mu           sync.Mutex
+	conn         net.Conn
+	closeTimer   *time.Timer
+	lastActivity time.Time
+}
+
+// Send sends data to server and ensures response length is greater than header length.
+func (mb *rtuovertcpTransporter) Send(aduRequest []byte) (aduResponse []byte, err error) {
+	mb.mu.Lock()
+	defer mb.mu.Unlock()
+
+	// Establish a new connection if not connected
+	if err = mb.connect(); err != nil {
+		return
+	}
+	// Set timer to close when idle
+	mb.lastActivity = time.Now()
+	mb.startCloseTimer()
+	// Set write and read timeout
+	var timeout time.Time
+	if mb.Timeout > 0 {
+		timeout = mb.lastActivity.Add(mb.Timeout)
+	}
+	if err = mb.conn.SetDeadline(timeout); err != nil {
+		return
+	}
+	// Send data
+	mb.logf("modbus: sending % x", aduRequest)
+	if _, err = mb.conn.Write(aduRequest); err != nil {
+		return
+	}
+	// Read header first
+	function := aduRequest[1]
+	functionFail := aduRequest[1] & 0x80
+	bytesToRead := calculateResponseLength(aduRequest)
+	time.Sleep(mb.calculateDelay(len(aduRequest) + bytesToRead))
+
+	var n int
+	var n1 int
+	var data [rtuMaxSize]byte
+	//We first read the minimum length and then read either the full package
+	//or the error package, depending on the error status (byte 2 of the response)
+	n, err = io.ReadAtLeast(mb.conn, data[:], rtuMinSize)
+	if err != nil {
+		return
+	}
+	//if the function is correct
+	if data[1] == function {
+		//we read the rest of the bytes
+		if n < bytesToRead {
+			if bytesToRead > rtuMinSize && bytesToRead <= rtuMaxSize {
+				if bytesToRead > n {
+					n1, err = io.ReadFull(mb.conn, data[n:bytesToRead])
+					n += n1
+				}
+			}
+		}
+	} else if data[1] == functionFail {
+		//for error we need to read 5 bytes
+		if n < rtuExceptionSize {
+			n1, err = io.ReadFull(mb.conn, data[n:rtuExceptionSize])
+		}
+		n += n1
+	}
+
+	if err != nil {
+		return
+	}
+	aduResponse = data[:n]
+	mb.logf("modbus: received % x\n", aduResponse)
+	return
+}
+
+func (mb *rtuovertcpTransporter) calculateDelay(chars int) time.Duration {
+	return time.Duration(750*chars+1750) * time.Microsecond
+}
+
+
+
+// Connect establishes a new connection to the address in Address.
+// Connect and Close are exported so that multiple requests can be done with one session
+func (mb *rtuovertcpTransporter) Connect() error {
+	mb.mu.Lock()
+	defer mb.mu.Unlock()
+
+	return mb.connect()
+}
+
+func (mb *rtuovertcpTransporter) connect() error {
+	if mb.conn == nil {
+		dialer := net.Dialer{Timeout: mb.Timeout}
+		conn, err := dialer.Dial("tcp", mb.Address)
+		if err != nil {
+			return err
+		}
+		mb.conn = conn
+	}
+	return nil
+}
+
+func (mb *rtuovertcpTransporter) startCloseTimer() {
+	if mb.IdleTimeout <= 0 {
+		return
+	}
+	if mb.closeTimer == nil {
+		mb.closeTimer = time.AfterFunc(mb.IdleTimeout, mb.closeIdle)
+	} else {
+		mb.closeTimer.Reset(mb.IdleTimeout)
+	}
+}
+
+// Close closes current connection.
+func (mb *rtuovertcpTransporter) Close() error {
+	mb.mu.Lock()
+	defer mb.mu.Unlock()
+
+	return mb.close()
+}
+
+// flush flushes pending data in the connection,
+// returns io.EOF if connection is closed.
+func (mb *rtuovertcpTransporter) flush(b []byte) (err error) {
+	if err = mb.conn.SetReadDeadline(time.Now()); err != nil {
+		return
+	}
+	// Timeout setting will be reset when reading
+	if _, err = mb.conn.Read(b); err != nil {
+		// Ignore timeout error
+		if netError, ok := err.(net.Error); ok && netError.Timeout() {
+			err = nil
+		}
+	}
+	return
+}
+
+func (mb *rtuovertcpTransporter) logf(format string, v ...interface{}) {
+	if mb.Logger != nil {
+		mb.Logger.Printf(format, v...)
+	}
+}
+
+// closeLocked closes current connection. Caller must hold the mutex before calling this method.
+func (mb *rtuovertcpTransporter) close() (err error) {
+	if mb.conn != nil {
+		err = mb.conn.Close()
+		mb.conn = nil
+	}
+	return
+}
+
+// closeIdle closes the connection if last activity is passed behind IdleTimeout.
+func (mb *rtuovertcpTransporter) closeIdle() {
+	mb.mu.Lock()
+	defer mb.mu.Unlock()
+
+	if mb.IdleTimeout <= 0 {
+		return
+	}
+	idle := time.Now().Sub(mb.lastActivity)
+	if idle >= mb.IdleTimeout {
+		mb.logf("modbus: closing connection due to idle timeout: %v", idle)
+		mb.close()
+	}
+}
